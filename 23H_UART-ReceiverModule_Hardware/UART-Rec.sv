1c4973b745a8ed926e7fe53b000edcd7fe97b173
Tomek <tomek12912@gmail.com>
2025-02-21 16:12:24 +0100

    Clean coding part 2

--- 23H_UART-ReceiverModule_Hardware/UART-Rec.sv
+++ 23H_UART-ReceiverModule_Hardware/UART-Rec.sv
@@ -23,51 +23,60 @@ module UART_Rec(
 	case(e_state)
 
 	IDLE: begin
-    i = 0;
+
 	if(rx != rx_z1) begin
 		e_state = START;
         frame_ctr = frame_ctr + 1;
         clock_ticks = 0;
+        i = 0;
 		end
 	end
 
     START: begin
-       clock_ticks = clock_ticks + 1;
 
        if(clock_ticks == int'(BIT_clk)) begin
             e_state = DATA;
             clock_ticks = 0;
        end
+
+       else
+            clock_ticks = clock_ticks + 1;
     end
 
 	DATA: begin
-		clock_ticks = clock_ticks + 1;
-        
+
         if(clock_ticks == int'(0.5 * BIT_clk)) begin
             data[i] = rx;
             i = i + 1;
+            clock_ticks = clock_ticks + 1;
         end
 
-        if(clock_ticks == int'(BIT_clk)) begin
+        else if(clock_ticks == int'(BIT_clk)) begin
             clock_ticks = 0;
             tick_tock = ~tick_tock;
         end
 
-        if(i == 8) begin
+        else if(i == 8) begin
             e_state = STOP;
             clock_ticks = 0;
             data_valid = 1;
         end
+
+        else
+            clock_ticks = clock_ticks + 1;
 		
 	end
 
     STOP: begin
-        clock_ticks = clock_ticks + 1;
-        data_valid = 0;       
-
+              
         if(clock_ticks == int'(5 * BIT_clk)) begin
             e_state = IDLE;
             clock_ticks = 0;
+        end
+
+        else begin
+            clock_ticks = clock_ticks + 1;
+            data_valid = 0; 
         end
     end
 
